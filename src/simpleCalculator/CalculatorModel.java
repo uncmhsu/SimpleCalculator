3daf3a6651a686a629c24d09fd2caf6b2c259bec
MichaelSu777 <msu242979@gmail.com>
2019-12-02 23:56:21 -0500

    Created the model for the simple calculator application

--- src/simpleCalculator/CalculatorModel.java
+++ src/simpleCalculator/CalculatorModel.java
@@ -1,5 +1,13 @@
 package simpleCalculator;
 
 public class CalculatorModel {
+	private int calculationValue;
 
+	public void addTwoNumbers(int firstNumber, int secondNumber) {
+		calculationValue = firstNumber + secondNumber;
+	}
+
+	public int getCalculationValue() {
+		return calculationValue;
+	}
 }
